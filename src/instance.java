627dc70ead841196b6b5c7183f6773e080e9407b
Sachin <sachin.kumar_cs20@gla.ac.in>
2021-09-14 21:58:35 +0530

    commit

---
+++ src/instance.java
@@ -0,0 +1,17 @@
+//Q4. Design an application in java that contains a clain t tor at the time of its object creation.
+// The output should be the name and percentage of the topper.
+//
+import java.util.*;
+public class instance {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("entre the first  number ");
+         int a=sc.nextInt();
+        System.out.println("enter the second number and find");
+        int b=sc.nextInt();
+        int c;
+         c=a+b;
+        System.out.println("this is the sum of given number "+c);
+    }
+
+}
