47fb26dabdb9fe0ef1b8684197775758f136dd45
Sachin <sachin.kumar_cs20@gla.ac.in>
2021-09-19 15:27:48 +0530

    maxnumber

---
+++ src/Addtion.java
@@ -0,0 +1,17 @@
+ public class Addtion {
+    int a;
+    int b;
+    Addtion(){
+        a=90;
+        b=80;
+    }
+    int ADD(){
+        return a-b;
+    }
+
+     public static void main(String[] args) {
+         Addtion obj=new Addtion();
+         obj.ADD();
+         System.out.println("the sum of two number a and b"+"\t"+obj.ADD());
+     }
+}
