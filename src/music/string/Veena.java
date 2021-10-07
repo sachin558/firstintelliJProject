e56b5df38b1ca27c81ed1455c9ceb530e86dfbdc
Sachin <sachin.kumar_cs20@gla.ac.in>
2021-10-07 18:51:12 +0530

    commit

---
+++ src/music/string/Veena.java
@@ -0,0 +1,10 @@
+package music.string;
+
+import music.Playboy;
+
+public  class Veena implements Playboy {
+    @Override
+    public void play() {
+        System.out.println("veena play");
+    }
+}
