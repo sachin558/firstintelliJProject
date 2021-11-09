9f9e4ddebf4f21b98341b7059e9af67b602d2b99
Sachin <sachin.kumar_cs20@gla.ac.in>
2021-11-09 14:28:48 +0530

    commit

--- src/ConvertIntegertoBinary.java
+++ src/ConvertIntegertoBinary.java
@@ -6,8 +6,8 @@ public class ConvertIntegertoBinary {
         Scanner sc=new Scanner(System.in);
         System.out.println("entre the integer value  between 1 to 255");
         i=sc.nextInt();
-        String re=Integer.toBinaryString(i);
-        System.out.println("this the integer number"+" = "+i+" and this is the binarary equvelent "+" = "+re);
+        String re=Integer.toOctalString(i);
+        System.out.println("this the integer number"+" = "+i+" and this is the octal equvelent of the given number "+" = "+re);
     }
 
     public static void main(String[] args) {
