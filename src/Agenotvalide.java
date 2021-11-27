f292d89ebc442f99178bcf61b07afb76c8e9357d
Sachin <sachin.kumar_cs20@gla.ac.in>
2021-11-27 10:16:32 +0530

    commit

--- src/Agenotvalide.java
+++ src/Agenotvalide.java
@@ -12,9 +12,11 @@ class UserException{
         Scanner obj=new Scanner(System.in);
         System.out.println("entre the command line argument ");
         try {
-            System.out.println("name of the person "+args[0]);
-            age=Integer.parseInt(args[1]);
+            name= obj.nextLine();
+            System.out.println("entre teh age of the person ");
+            age=Integer.parseInt(obj.nextLine());
             if(age>=18 && age<60){
+                System.out.println("name of the person "+name);
                 System.out.println("this is the age of the person "+age );
             }
             else {
