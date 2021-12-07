588ad014a46feaffe4aa93e47f005a396748bf54
Sachin <sachin.kumar_cs20@gla.ac.in>
2021-12-07 23:20:03 +0530

    commit

--- src/RegularExperationQ1.java
+++ src/RegularExperationQ1.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExperationQ1 {
     public static void main(String[] args) {
-        Pattern P=Pattern.compile("[b]+");
-        Matcher M=P.matcher("acdhbbdjfhhbbkhbb");
+        Scanner sc=new Scanner(System.in);
+        Pattern P=Pattern.compile("[baa]+");
+        System.out.println("entre the target string ");
+        Matcher M=P.matcher(sc.nextLine());
         int co=0;
         while (M.find()){
             co++;
